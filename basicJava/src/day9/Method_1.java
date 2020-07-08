7914cedec26e089fe92e7a165266edb1f0be0daf
User_1000 <User_1000@DESKTOP-VU6MUU4>
2020-07-08 17:02:49 +0900

    test용

---
+++ basicJava/src/day9/Method_1.java
@@ -0,0 +1,26 @@
+package day9;
+
+public class Method_1 {
+	
+	//클래스(class ) :field + method
+	//맴버 변수 : 속성,필드s
+	//맴버 함수 : 메소드s
+	
+	public static void test() {
+		System.out.println("test() 메소드 왔다감!!");
+	}
+
+	public static void main(String[] args) { //메소드 헤더
+		// TODO Auto-generated method stub
+
+		
+		
+		System.out.println("main()메소드 :프로그램의 시작"); // 메소드 바디
+		
+		test();//메서드 호출문 : CALL
+		System.out.println("test()메소드 호출후 돌아옴!!");
+		
+		
+	}//main end
+
+}//class end
