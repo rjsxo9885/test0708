86fed3b47a5e0459ddbdbeafa59898a6a7d9820b
User_1000 <User_1000@DESKTOP-VU6MUU4>
2020-07-14 18:15:00 +0900

    test

--- basicJava/src/day12/ClassTest.java
+++ basicJava/src/day12/ClassTest.java
@@ -1,10 +1,44 @@
 package day12;
 
+
+class Sunjuk{
+	int kor,eng,math;
+	
+	int avg;
+	
+	public void tensu(int a,int b,int c) {
+		
+		
+		kor = a;
+		eng = b;
+		math = c;
+		
+	}
+	
+	public void calc() {
+		avg = (kor+eng+math)/3;
+	}
+	public void disp() {
+		System.out.println(kor);
+		System.out.println(eng);
+		System.out.println(math);
+		System.out.println(avg);
+	}
+}
+
 public class ClassTest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Sunjuk nl = new Sunjuk();
+		nl.tensu(100,90,80);
+		nl.calc();
+		System.out.println(nl.kor);
+		System.out.println(nl.eng);
+		System.out.println(nl.math);
+		System.out.println(nl.avg);
+		
+		
 	}
 
 }
